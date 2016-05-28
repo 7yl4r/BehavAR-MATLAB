4336f1c6a08e4e10cdb5a3991091f18c2d5a3ca3
7yl4r <murray.tylar@gmail.com>
2016-05-28 04:29:35 -0400

    added titles

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 % fake data:
-exogeneous_U = mockInputData(1000, 'random');  
+exogeneous_U = mockInputData(500, 'random');  
 mockSys = idpoly([1, 0, 0], [.2, .5, .1]);  % mock arx model
 outputData_Y = sim(mockSys, exogeneous_U);
 
@@ -7,14 +7,17 @@ outputData_Y = sim(mockSys, exogeneous_U);
 [trainData, testData] = randomSelectionSplit(outputData_Y, exogeneous_U);
 
 plot(trainData);
+title('Training Data');
 figure;
 plot(testData);
+title('Validation Data');
 
 % train the arx model
 [ sys, Y, X ] = behavARX( trainData, 2 );
 
 figure;
 compare(iddata(outputData_Y, exogeneous_U), sys);
+title('Model vs Data');
 
 Adiff = mockSys.a - sys.a;
 Bdiff = mockSys.b - sys.b;
